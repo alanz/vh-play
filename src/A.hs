09e2e451bdf6038fd7851e2ea9aa02ab10e65da4
Alan Zimmerman <alan.zimm@gmail.com>
2012-07-05 23:43:04 +0200

    More digging into structure

--- src/A.hs
+++ src/A.hs
@@ -4,6 +4,7 @@
 import GHC
 import Outputable
 import Bag
+import SrcLoc
 import GHC.Paths ( libdir )
 --GHC.Paths is available via cabal install ghc-paths
 
@@ -47,9 +48,98 @@ mymain = do
   res <- example
   let lhsbinds = bagToList res
   -- putStrLn $ showSDoc ( ppr res )
-  putStrLn $ showSDoc ( ppr lhsbinds )
+  -- putStrLn $ showSDoc ( ppr $ unLoc $ head lhsbinds )
+  -- putStrLn $ myShow $ unLoc $ head lhsbinds
+  putStrLn $ concatMap (\b -> "," ++ (myShow $ unLoc b)) lhsbinds
+
+myShow :: (Show a, Show t) => HsBindLR a t -> String
+myShow x = case x of
+  (FunBind  id isInfix matches _coercion fvs _tickish ) -> "(FunBind:" ++ (show (unLoc id)) ++ "[" ++ (showMatchGroup matches) ++ "]" ++ ")"
+  (PatBind  a b c d e   ) -> "PatBind"
+  (VarBind  a b c       ) -> "VarBind"
+  (AbsBinds tvs evvars exports evbinds binds) -> "AbsBinds" ++ (sie tvs) ++ (sie evvars) ++ (concatMap showExport exports) {- ++ (sie evbinds) -}  ++ (concatMap (\b -> myShow $ unLoc b) $ bagToList binds)
+
+sie []  = "[]"
+sie xs  = "[x]"
+
+showExport (ABE poly mono wrap prags) = "(ABE " ++ (show poly) ++ "," ++ (show mono) ++ ")"
+
+showBinds b = "foo"
+
+showMatchGroup (MatchGroup ms ptc) = "(" ++ (concatMap (\m -> showMatch $ unLoc m) ms) ++ ")"
+
+showMatch (Match lpats mt rhs) = "(Match [" ++ concatMap (\l -> showPat $ unLoc l) lpats ++ "] " ++ (showGRHSs rhs) ++ ")"
+
+showPat x = case x of
+  VarPat id -> "(VarPat " ++ (show id) ++ ")"
+  _ -> "Unknown Pat"
+
+showGRHSs (GRHSs guards lb) = "(GRHSs [" ++ (concatMap (\g -> showGuard $ unLoc g) guards) ++ "] " ++ (showLocalBinds lb) ++ " )"
+
+showGuard (GRHS lstmts expr) = "(GRHS [" ++ (concatMap (\l -> showStmtLR $ unLoc l) lstmts) ++ "] "++ (showHsExpr $ unLoc expr) ++ ")"
+
+showStmtLR x = "StmtLR"
+
+showHsExpr :: (Show a) => HsExpr a -> String
+showHsExpr e = case e of
+  HsVar id -> "(HsVar " ++ (show id) ++ ")"
+  HsIPVar _id -> "(HsIPVar)"
+  HsOverLit (OverLit v rb w t) -> "(HsOverLit " ++ (showOverLit v)++ ")"
+  HsLit lit -> "(HsLit)"
+  HsLam mg -> "(HsLam)"
+  HsApp e1 e2 -> "(HsApp)"
+  OpApp e1 e2 fixity e3 -> "(OpApp)"
+  NegApp e1 se1 -> "(NegApp)"
+  HsPar e1 -> "(HsPar)"
+  SectionL e1 e2 -> "(SectionL)"
+  SectionR e1 e2 -> "(SectionR)"
+  ExplicitTuple tuparg boxity -> "(ExplicitTuple)"
+  HsCase e1 mg -> "(HsCase)"
+  HsIf mse e1 e2 e3 -> "(HsIf)"
+  HsLet lb e1 -> "(HsLet)"
+  HsDo n ls t -> "(HsDo)"
+  ExplicitList t e1 -> "(ExplicitList)"
+  ExplicitPArr t e1 -> "(ExplicitPArr)"
+  RecordCon li te rb -> "(RecordCon)"
+  RecordUpd e1 rb dcs ts1 ts2 -> "(RecordUpd)"
+  ExprWithTySig e1 t -> "(ExprWithTySig)"
+  ExprWithTySigOut e1 t -> "(ExprWithTySigOut)"
+  ArithSeq te si -> "(ArithSeqInfo)"
+  PArrSeq te si -> "(PArrSeq)"
+  HsSCC fs e1 -> "(HsSCC)"
+  HsCoreAnn fs e1 -> "(HsCoreAnn)"
+  HsBracket b -> "(HsBracket)"
+  HsBracketOut b ps -> "(HsBracketOut)"
+  HsSpliceE s -> "(HsSplice)"
+  HsQuasiQuoteE qq -> "(HsQuasiQuote)"
+  HsProc lp lc -> "(HsProc)"
+  HsArrApp e1 e2 t at b -> "(HsArrAppType)"
+  HsArrForm e1 mf lcs -> "(HsArrForm)"
+  HsTick ti e1 -> "(HsTick)"
+  HsBinTick i1 i2 e1 -> "(HsBinTick)"
+  HsTickPragma a b -> "(HsTickPragma)"
+  EWildPat -> "(EWildPat)"
+  EAsPat lid e1 -> "(EAsPat)"
+  EViewPat e1 e2 -> "(EViewPat)"
+  ELazyPat e1 -> "(ELazyPat)"
+  HsType lt -> "(HsType)"
+  HsWrap w e1 -> "(HsWrap)"
+  -- _         -> "(unk HsExpr)"
 
 
+
+showOverLit (HsIntegral i) = show i
+showOverLit (HsFractional fl) = show fl
+showOverLit (HsIsString fs) = show fs
+
+showLocalBinds x = case x of
+  HsValBinds vb -> "HsValBinds"
+  HsIPBinds ipb -> "HsIPBinds"
+  EmptyLocalBinds -> "EmptyLocalBinds"
+
+-- getBind :: Located e -> e
+-- getBind (GenLocated span b) = b
+-- getBind x = undefined
 
 
 
